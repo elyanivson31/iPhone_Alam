c303ca9e3a5476970a8e1d108acc4260153a52d0
Yaniv Eliav <eliav.yaniv@gmail.com>
2017-10-12 22:16:57 +0300

    Commit

---
+++ iPhone7_ALAM_DUTY/Email.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace iPhone7_ALAM_DUTY
+{
+    class Email
+    {
+        public static void sendEmail()
+        {
+            var fromAddress = new MailAddress("elyanivson@gmail.com", "Elyanivson");
+            var toAddress = new MailAddress("eliav.yaniv@gmail.com", "Yaniv Eliav");
+            const string fromPassword = "77777777";
+            const string subject = "Out of Stock";
+            const string body = "still out of stock!!";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
---
+++ iPhone7_ALAM_DUTY/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPhone7_ALAM_DUTY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDataFromURL();
+        }
+        public static void GetDataFromURL()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString("http://dutyfree.alm.co.il/dutyfree/Category.aspx?cid=1&vmenu=5");
+                var IndexofIphone7 = htmlCode.IndexOf("iPhone 7 32GB");
+                var StringContainsOutOfStock = htmlCode.Substring(IndexofIphone7, 248);
+                if (StringContainsOutOfStock.Contains("outOfStock"))
+                    Console.WriteLine("Still out of stock");
+                else
+                    Email.sendEmail();
+
+            }
+        }
+    }
+}
